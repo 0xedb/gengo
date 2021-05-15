6a2e2e894c1520cd5d1c83897e7b57e155219881
0xedb <edohbruno@gmail.com>
2021-05-15 07:57:55 +0000

    more tokens

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -30,19 +30,31 @@ func (l *Lexer) NextToken() token.Token {
 
 	l.eatWhitespace()
 
-	switch l.ch {
-	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+	switch l.ch { 
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '!':
+		tok = newToken(token.BANG, l.ch)
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '<':
+		tok = newToken(token.LT, l.ch)
+	case '>':
+		tok = newToken(token.GT, l.ch)
 	case ';':
-		tok = newToken(token.SEMICOLON, l.ch)
+		tok = newToken(token.SEMICOLON, l.ch) 
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch) 
 	case '(':
 		tok = newToken(token.LPAREN, l.ch)
 	case ')':
 		tok = newToken(token.RPAREN, l.ch)
 	case ',':
-		tok = newToken(token.COMMA, l.ch)
-	case '+':
-		tok = newToken(token.PLUS, l.ch)
+		tok = newToken(token.COMMA, l.ch) 
 	case '{':
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
